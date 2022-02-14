e09b9b2d6c1920f2de51619b0e3a70d03922f4ad
YellowFlash2012 <cyclop9@hotmail.com>
2022-02-14 13:41:55 +0100

    modified the config file to facilitate heroku deployment

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
 
